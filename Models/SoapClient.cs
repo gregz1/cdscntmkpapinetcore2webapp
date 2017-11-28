5e181905f63c3c8d765cce8766b69ae006c1eb24
gregory.tall <gregory.tall@ext.cdiscount.com>
2017-11-28 14:57:21 +0100

    test proxy

--- Models/SoapClient.cs
+++ Models/SoapClient.cs
@@ -27,6 +27,12 @@ namespace cdscntmkpapinetcore2webapp.Models
             MarketplaceAPIServiceClient Client = new MarketplaceAPIServiceClient();            
             Client = new MarketplaceAPIServiceClient(MarketplaceAPIServiceClient.EndpointConfiguration.BasicHttpBinding_IMarketplaceAPIService, "https://wsvc.preprod-cdiscount.com/MarketplaceAPIService.svc");
             var b = Client.Endpoint.Binding as System.ServiceModel.BasicHttpBinding;
+
+            b.SendTimeout = TimeSpan.FromMinutes(10);
+            b.ReceiveTimeout = TimeSpan.FromMinutes(10);
+            b.OpenTimeout = TimeSpan.FromMinutes(10);
+            b.CloseTimeout = TimeSpan.FromMinutes(10);
+            
             //string proxyUrl = Environment.GetEnvironmentVariable("QUOTAGUARDSTATIC_URL");
 			string proxyUrl = "http://m98js9u10vrq7m:4RBT1Hml9SL8uipUUXJ9R8iCDg@eu-west-1-babbage.quotaguard.com:9293";
             System.Uri proxyUri = new System.Uri(proxyUrl);
