636a35a3117ae1b272025a70d578a396435ce557
gregory.tall <gregory.tall@ext.cdiscount.com>
2019-10-08 18:57:11 +0200

    fbc

--- Models/FBCManager/CreateExternalOrderRequest.cs
+++ Models/FBCManager/CreateExternalOrderRequest.cs
@@ -9,11 +9,9 @@ namespace cdscntmkpapinetcore2webapp.Models.FBCManager
     public class CreateExternalOrderRequest : Request
     {
         public OrderIntegrationRequest _MyOrderIntegrationRequest { get; set; }
-        public string rootFolder { get; set; }
         public CreateExternalOrderRequest()
         {
             _MyOrderIntegrationRequest = new OrderIntegrationRequest();            
-            rootFolder = System.AppDomain.CurrentDomain.BaseDirectory;
         }
     }
 }
